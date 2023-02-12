9f8c527298eba3214fc8a246bd10272e499f6b0a
Htifano <91378921+Kuretha@users.noreply.github.com>
2023-02-12 23:01:53 +0700

    submission by Habellio

---
+++ README.md
@@ -0,0 +1,10 @@
+# Deskripsi Program
+
+Program ini berisikan html dan css tentang recipe beberapa makanan, untuk melihat program silahkan pencet tulisan yang tertera diatas gambar, web ini telah ditambahkan api themealdb dengan fitur searchbar
+
+## Identitas Pembuat
+
+Nama: Habellio Tifano
+Nim: 001202200004
+Univ: President University
+ID GDSC: 220010071
---
+++ index.js
@@ -0,0 +1,114 @@
+const API_URL = "https://www.themealdb.com/api/json/v1/1/";
+const SEARCH_BY_NAME = "search.php?";
+const MEAL_DETAILS = "lookup.php?";
+
+class Search {
+  constructor(form) {
+    this.form = form;
+    this.results = document.querySelector(".search_res");
+  }
+
+  initialize() {
+    console.log(this.form);
+    this.search();
+  }
+
+  search() {
+    const searchField = this.form.querySelector("input[type=text]");
+    if (searchField) {
+      this.form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        fetch(
+          `${API_URL}${SEARCH_BY_NAME}` +
+            new URLSearchParams({
+              s: searchField.value.replace(/\s+/g, ""),
+            }),
+          {
+            method: "GET",
+          }
+        )
+          .then((response) => {
+            response.json().then((data) => {
+              if (data.meals) {
+                this.displaySearchResults(data.meals);
+              } else {
+                this.results.innerHTML = "No results";
+              }
+            });
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+      });
+    }
+  }
+
+  displaySearchResults(meals) {
+    this.results.innerHTML = "";
+    meals.forEach((meal) => {
+      const mealElement = document.createElement("div");
+      mealElement.innerHTML = `
+                <h2>${meal.strMeal}</h2>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" width="500px"/>
+            `;
+      mealElement.addEventListener("click", () => {
+        this.displayMealDetails(meal.idMeal);
+      });
+      this.results.appendChild(mealElement);
+    });
+  }
+
+  displayMealDetails(id) {
+    fetch(
+      `${API_URL}${MEAL_DETAILS}` +
+        new URLSearchParams({
+          i: id,
+        }),
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        response.json().then((data) => {
+          if (data.meals) {
+            const meal = data.meals[0];
+            let ingredients = [];
+            for (let i = 1; i <= 20; i++) {
+              if (meal[`strIngredient${i}`]) {
+                ingredients.push(
+                  `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+                );
+              }
+            }
+            const recipeElement = document.createElement("div");
+            recipeElement.innerHTML = `
+                        <h2>${meal.strMeal}</h2>
+                        <img src="${meal.strMealThumb}" alt="${
+              meal.strMeal
+            }" width="500px">
+                        <h3>Ingredients:</h3>
+                        <ul>
+                            ${ingredients
+                              .map((ingredient) => `<li>${ingredient}</li>`)
+                              .join("")}
+                        </ul>
+                        <h3>Instructions:</h3>
+                        <p>${meal.strInstructions}</p>
+                    `;
+            this.results.innerHTML = "";
+            this.results.appendChild(recipeElement);
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const search = new Search(form);
+
+  search.initialize();
+});
